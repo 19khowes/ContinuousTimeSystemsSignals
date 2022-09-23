a1be67d5554e663cb0287f25c206e8ea9c718c7d
19khowes <19khowes@gmail.com>
2022-09-22 20:26:22 -0600

    iteritively compute next solutions

--- Assignment1/odeSolve.cpp
+++ Assignment1/odeSolve.cpp
@@ -2,45 +2,74 @@
 #include <fstream>
 
 using namespace std;
-void getCoefficients(int order, float *coefficients);
-void getInitials(int order, float *initials);
+void getCoefficients(int order, double *coefficients);
+void getInitials(int order, double *initials);
 float getDeltaT();
 float getStartTime();
 float getEndTime();
-void initializeX(int order, float *x, float *initials);
-void initializeA(int order, float **A, float *coeffiecients);
+void initializeX(int order, double *x, double *initials);
+void initializeA(int order, double dt, double **A, double *coeffiecients);
+void matrixVecMult(int order, double **A, double *x, double *newx);
 
 int main() {
+    // open file to write output to
+    ofstream outfile("output.txt");
     // ask user for the order of differential equation and store in order
     int order = 1;
     cout << "Enter the order of the differential equation: ";
     cin >> order;
     
     // get user input (coefficients, intials, delta t, start time, and end time)
-    float coefficients[order];
+    double coefficients[order];
     getCoefficients(order, coefficients);
-    float initials[order];
+    double initials[order];
     getInitials(order, initials);
     float dt = getDeltaT();
     float startTime = getStartTime();
     float endTime = getEndTime();
 
     // create x vector (start with intial conditions)
-    float x[order];
+    double x[order];
     initializeX(order, x, initials);
-    // create matrix A + I
-    float **A;
-    A = new float* [order];
+    // create matrix I + A(dt)
+    double **A;
+    A = new double* [order];
     for (int i = 0; i < order; i++) {
-        A[i] = new float[order];
+        A[i] = new double[order];
     }
-    initializeA(order, A, coefficients);
+    initializeA(order, dt, A, coefficients);
 
-    
+    // set up time and counting variables
+    float runTime = startTime - endTime;
+    float numPoints = runTime / dt;
+    double newx[order];
+    for (float t = startTime; t <= endTime; t += dt) {
+        matrixVecMult(order, A, x, newx);
+        // write newx's x1 to file with time t
+        cout << "next solution: " << newx[0] << "\n";
+        outfile << t << " " << newx[0] << endl;
+        // change x = newx for next iteration
+        for (int i = 0; i < order; i++) {
+            x[i] = newx[i];
+        }
+    }  
+
+    // close output file 
+    outfile.close();
 }
 
+void matrixVecMult(int order, double **A, double *x, double *newx) {
+    double sum;
+    for (int i = 0; i < order; i++) {
+        sum = 0;
+        for (int j = 0; j < order; j++) {
+            sum += A[i][j] * x[j];
+        }
+        newx[i] = sum;
+    }
+}
 
-void initializeX(int order, float *x, float *initials) {
+void initializeX(int order, double *x, double *initials) {
     cout << "x vector: \n";
     for (int i = 0; i < order; i++) {
         x[i] = initials[i];
@@ -49,7 +78,7 @@ void initializeX(int order, float *x, float *initials) {
 
 }
 
-void initializeA(int order, float **A, float *coeffiecients) {
+void initializeA(int order, double dt, double **A, double *coeffiecients) {
     for (int i = 0; i < order; i++) {
         for (int j = 0; j < order; j++) {
             if (j == (i + 1)) {
@@ -59,6 +88,8 @@ void initializeA(int order, float **A, float *coeffiecients) {
             if (i == (order - 1)) {
                 A[i][j] = -1 * coeffiecients[j];
             }
+            // multiply by dt
+            A[i][j] = A[i][j] * dt;
             // add identity matrix now
             if (i == j) {
                 A[i][j] = A[i][j] + 1;
@@ -67,7 +98,7 @@ void initializeA(int order, float **A, float *coeffiecients) {
     }
 
     // print A matrix
-    cout << "Matrix A + I:\n";
+    cout << "Matrix I + A(dt):\n";
     for (int i = 0; i < order; i++) {
         for (int j = 0; j < order; j++) {
             cout << A[i][j] << " ";
@@ -77,7 +108,7 @@ void initializeA(int order, float **A, float *coeffiecients) {
 }
 
 
-void getCoefficients(int order, float *coefficients) {
+void getCoefficients(int order, double *coefficients) {
     // ask user for constant coeffiecients
     cout << "Enter coefficients from a_" << order-1 << " down to a_0:\n";
     int index = 0;
@@ -92,7 +123,7 @@ void getCoefficients(int order, float *coefficients) {
     }
 }
 
-void getInitials(int order, float *initials) {
+void getInitials(int order, double *initials) {
     cout << "\n\nEnter initial conditions:\n";
     for (int i = 0; i < order; i++) {
         // ask for specific y'(t) inital condition
